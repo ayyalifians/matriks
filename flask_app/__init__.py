5500b9c4b406bd9f66fa10fe8af1bf5db5603219
ayyalifians <alifiannisa01@gmail.com>
2025-11-03 11:47:44 +0700

    tanpa data baru

---
+++ flask_app/__init__.py
@@ -0,0 +1,21 @@
+import os
+import csv
+from regression.linear_regression import LinearRegression
+
+class StudentModel:
+    """
+    Model untuk memuat dataset mahasiswa dan
+    melakukan prediksi menggunakan Linear Regression.
+    """
+
+    def __init__(self, data_path=None):
+        # Tentukan path absolut berdasarkan lokasi file ini
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        if data_path is None:
+            self.data_path = os.path.join(base_dir, "..", "data", "student_scores.csv")
+        else:
+            self.data_path = data_path
+
+        self.X = []
+        self.y = []
+        self.model = LinearRegression()
